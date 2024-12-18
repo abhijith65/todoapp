559297e5f780dcd875805c3037455e5ca09db6ba
abhijith65 <65abhijith@gmail.com>
2024-12-18 15:32:04 +0530

    dec18.

--- lib/views/widget/button.dart
+++ lib/views/widget/button.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/constants/colors.dart';
+import 'package:todo_app/constants/textstyle.dart';
 
 class LevelButton extends StatelessWidget {
    LevelButton(this.name);
-   String? name;
+   final String? name;
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color:MyColors.lvlcolor['$name'],
-      child: Padding(
-        padding: const EdgeInsets.all(2.0),
-        child: Text(name??''),
+    return Container(
+      height: 25,
+      child: Card(
+        color:MyColors.lvlcolor['$name']?.withValues(alpha: .5),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(2.0),
+            child: Text(name??'',style: MyTextThemes.levelTextStyle,),
+          ),
+        ),
       ),
     );
   }
